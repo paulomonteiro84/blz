fe9a73276dfd5622f2aa56329daba5936bbdedf7
paulomonteiro84 <paulomonteiro84@gmail.com>
2017-04-07 02:04:34 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+    <link rel="stylesheet" href="css/foundation.min.css">
+    <link rel="stylesheet" href="css/app.css?v=<?=time()?>">
+
+    <title>Document</title>
+</head>
+<body>
+<div class="row">
+    <div class="small-12 topbar columns no-padding">
+        <div class="row collapse">
+            <div class="small-12 columns text-right social">Social</div>
+        </div>
+    </div>
+</div>
+<script src="js/app.js"></script>
+</body>
+</html>
