82f6a432b49254e866d635738cd3f01678d9b87b
P-Laptop <paulomonteiro84@gmail.com>
2017-04-09 20:18:46 +0200

    slider added - working

--- index.php
+++ index.php
@@ -7,23 +7,44 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
     <link rel="stylesheet" href="css/foundation.min.css">
+    <link rel="stylesheet" href="css/slider.css">
     <link rel="stylesheet" href="css/app.css?v=<?=time()?>">
+
 
     <title>Document</title>
 </head>
 <body>
 <div class="row">
-    <div class="small-12 topbar columns no-padding">
+    <div class="small-12 topbar columns no-padding social">
         <div class="row collapse">
-            <div class="small-6 columns text-left"></div>
-            <div class="small-12 columns text-left social"></div>
+            <div class="small-6 columns text-left">Space</div>
+            <div class="small-6 columns text-right">Social</div>
         </div>
     </div>
-    <div class="small-12 large-3 columns">
-        <div class="logo"></div>
-        <div class="menu"></div>
+    <div class="small-12 large-3 columns text-left">
+        <div class="logo">BODY LASER</div>
+    </div>
+    <div class="small-12 large-9 columns topnav text-right">
+        <ul>
+            <li>Home</li>
+            <li>Promoções</li>
+            <li>Tratamentos</li>
+            <li>Localização</li>
+        </ul>
     </div>
 </div>
-<script src="js/app.js"></script>
+<div class="row">
+    <div class="small-12 columns collapse">
+        <div class="carousel">
+            <div class="carousel-cell"></div>
+            <div class="carousel-cell"></div>
+            <div class="carousel-cell"></div>
+
+        </div>
+    </div>
+</div>
+<script src="js/vendor/jquery.js"></script>
+<script src="js/vendor/slider.js"></script>
+<script src="js/app.js?v=<?=time()?>"></script>
 </body>
 </html>
---
+++ js/app.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('.carousel').flickity({
+        cellSelector: '.carousel-cell',
+        pageDots: true
+    }).fadeIn();
+
+});
